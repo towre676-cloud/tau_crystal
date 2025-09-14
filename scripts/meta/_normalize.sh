8d49a090c9d9605b4352cee9eb263342747b3632
Cody <towre676@gmail.com>
2025-09-13 22:22:14 -0400

    feat(sheaf): τ‑Sheaf reflection digest + manifest stamp

--- scripts/meta/_normalize.sh
+++ scripts/meta/_normalize.sh
@@ -2,5 +2,5 @@
 set -Eeuo pipefail; set +H
 in="$1"; out="$2"
 [ -n "$in" ] && [ -n "$out" ] || { echo "[err] usage: _normalize.sh <in> <out>" >&2; exit 2; }
-tr -d "\r" < "$in" | sed -E "s/[[:space:]]+$//" > "$out".lf
+tr -d "\r" < "$in" | sed -E "s/[ \t]+$//" > "$out".lf
 mv "$out".lf "$out"
