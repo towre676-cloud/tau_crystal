f1b6a2e10e644e007498fecd17f45bed09f9d6e9
Cody <you@example.com>
2025-09-20 12:19:07 -0400

    lean: ASCII-only DeltaComplex; remove unicode id; clean LeafGroup

--- Tau/LeafGroup.lean
+++ Tau/LeafGroup.lean
@@ -4,8 +4,8 @@ abbrev LeafID := String
 abbrev LeafGroup := LeafID → Int
 
 def leafGroupZero : LeafGroup := fun _ => 0
-def leafGroupAdd (f g : LeafGroup) : LeafGroup := fun x => f x + g x
-def leafGroupNeg (f : LeafGroup) : LeafGroup := fun x => - (f x)
+def leafGroupAdd  (f g : LeafGroup) : LeafGroup := fun x => f x + g x
+def leafGroupNeg  (f   : LeafGroup) : LeafGroup := fun x => - (f x)
 
 def leafGroupL1Norm (f : LeafGroup) (support : List LeafID) : Nat :=
   support.foldl (fun acc x => acc + Int.natAbs (f x)) 0
@@ -14,7 +14,7 @@ def leafGroupIsZero (f : LeafGroup) (support : List LeafID) : Bool :=
   support.all (fun x => f x = 0)
 
 instance : Zero LeafGroup where zero := leafGroupZero
-instance : Add LeafGroup  where add  := leafGroupAdd
-instance : Neg LeafGroup  where neg  := leafGroupNeg
+instance : Add  LeafGroup where add  := leafGroupAdd
+instance : Neg  LeafGroup where neg  := leafGroupNeg
 
 end Tau
